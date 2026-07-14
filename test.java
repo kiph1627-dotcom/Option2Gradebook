14acd1c3ed3c54b845626fb1eb3449d0505a36ea
peckock123 <srikar.pochinapeddi@gmail.com>
2026-07-14 15:18:20 -0400

    Update test.java

--- test.java
+++ test.java
@@ -6,10 +6,17 @@ public class GradebookStudent
     private int id;
     private String name;
     private ArrayList<GradeItem> grades;
-    public GradebookStudent(int id, String name )
-    {
+    public GradebookStudent(int id, String name) {
+        if (id <= 0) {
+            throw new IllegalArgumentException("Student id must be > 0.");
+        }
+        if (name == null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException("Student name must not be null or empty.");
+        }
         this.id = id;
         this.name = name.trim();
         this.grades = new ArrayList<GradeItem>();
     }
+    
+    
 }
