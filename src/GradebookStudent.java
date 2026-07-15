4992aae365407281581df35ddf6ae0f3d756b996
Kian <kiph1627@gmail.com>
2026-07-15 14:57:09 -0400

    Resolved merge conflict

--- src/GradebookStudent.java
+++ src/GradebookStudent.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package src;
 
 
@@ -78,84 +77,4 @@ public class GradebookStudent
     
     
 }
-=======
-package src;
 
-
-import java.util.ArrayList;
-
-public class GradebookStudent
-{
-    private int id;
-    private String name;
-    private ArrayList<GradeItem> grades;
-    public GradebookStudent(int id, String name) {
-        if (id <= 0) {
-            throw new IllegalArgumentException("Student id must be > 0.");
-        }
-        if (name == null || name.trim().isEmpty()) {
-            throw new IllegalArgumentException("Student name must not be null or empty.");
-        }
-        this.id = id;
-        this.name = name.trim();
-        this.grades = new ArrayList<GradeItem>();
-    }
-
-    public int getID()
-    {
-        return id;
-    }
-    public String getName()
-    {
-        return name;
-    }
-    public void setName(String name)
-    {
-         if (name == null || name.trim().isEmpty()) 
-         {
-            throw new IllegalArgumentException("Student name must not be null or empty.");
-        }
-        this.name = name.trim();
-    }
-    public void addGrade(GradeItem grade)
-    {
-        if (grade == null) {
-        throw new IllegalArgumentException("Grade cannot be null.");
-        }
-        grades.add(grade);
-    }
-   public ArrayList<GradeItem> getGrades() 
-    { 
-    return new ArrayList<GradeItem>(grades); 
-    }
-
-    public double averageGrade()
-    {
-        if (grades.isEmpty())
-        {
-            return 0.0;
-        }
-        double total = 0.0;
-        for(GradeItem g : grades)
-        {
-            total += g.getScore();
-        }
-        return total/grades.size();
-    }
-      @Override
-    public String toString() {
-        String avgText;
-        if (grades.isEmpty()) {
-            avgText = "no grades yet";
-        } else {
-            avgText = String.format("%.2f", averageGrade());
-        }
-        return "ID: " + id + " | Name: " + name + " | Average: " + avgText;
-    }
-
-    
-
-    
-    
-}
->>>>>>> 73ec58d43c30330731b77a1749a2718d565a9e0a
